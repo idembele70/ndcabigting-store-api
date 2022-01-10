958a88014ce8a13771d35b237a57551a0d5674c8
ecabigting <ericcabigting@outlook.com>
2022-01-10 19:44:31 +0400

    added user registration under auth endpoints

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require("./routes/users")
+const authRoute = require("./routes/auth")
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ app.listen(process.env.PORT || 5000, () => {
 app.use(express.json());
 
 app.use("/api/users",userRoute);
+app.use("/api/auth",authRoute);
 
 app.get('/api/test',()=>{
     console.log('test')
